5b9b51789007d693ba9fce2bfa9d807105c5094b
Valisoa411 <99267101+Valisoa411@users.noreply.github.com>
2024-02-21 16:58:10 +0300

    renforce horaire control

--- models/Horaire.js
+++ models/Horaire.js
@@ -9,11 +9,42 @@ class Horaire {
     ) {
         this.idEmploye = idEmploye;
         this.jour = jour;
+        this.setPlage(debut, fin)
+    }
+
+    setPlage(debut, fin) {
+        const debutTime = new Date(`2000-01-01T${debut}`);
+        const finTime = new Date(`2000-01-01T${fin}`);
+        if (debutTime > finTime) {
+            throw new Error('Le début de l\'horaire doit être avant la fin');
+        }
         this.debut = debut;
         this.fin = fin;
     }
 
+    async checkAvailability() {
+        const pipeline = [
+            {
+                $match: {
+                    idEmploye: this.idEmploye,
+                    jour: Number(this.jour),
+                    $expr: {
+                        $and: [
+                            { $lt: ["$debut", this.fin] },
+                            { $gt: ["$fin", this.debut] }
+                        ]
+                    }
+                }
+            }
+        ];
+        const horaires = await HoraireModel.aggregate(pipeline);
+        if (horaires.length > 0) {
+            throw new Error('Plage horaire déjà utilisée');
+        }
+    }
+
     async insert() {
+        await this.checkAvailability();
         const newHoraireMongoose = new HoraireModel({ ...this });
         return await newHoraireMongoose.save();
     }
